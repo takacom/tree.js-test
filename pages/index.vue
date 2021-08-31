78f697692eba6abbd85b2e67a5c42d54984e8075
takacom <17254602+takacom@users.noreply.github.com>
2021-08-31 21:04:11 +0900

    時間表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,50 @@
 <template>
-  <Tutorial/>
+<section class="container">
+<h1>{{title}}</h1>
+<p>{{message}}</p>
+<hr>
+<pre>{{now}}</pre>
+</section>
 </template>
 
 <script>
-export default {}
+export default {
+  data: function() {
+    return {
+      title:'Hello',
+      message: 'this is message.',
+      now: 'wait...',
+    };
+  },
+  created: function() {
+    setInterval( () =>{
+      var d = new Date();
+      this.now = d.getHours()
+        + ':' + d.getMinutes()
+        + ':' + d.getSeconds();
+    }, 1000);
+  },
+};
 </script>
+
+<style>
+.container {
+  padding:5px 10px;
+}
+h1 {
+  font-size: 60pt;
+  color: #345980;
+}
+p {
+  padding-top: 5px;
+  font-size: 20pt;
+}
+pre {
+  padding: 10px;
+  font-size: 18pt;
+  background-color: #efefef;
+}
+hr {
+  margin: 10px 0px;
+}
+</style>
